02ca7de5e9f39390119637ed3d8a56bfd7eef514
louisrubin <miguel10.rbn@gmail.com>
2023-09-01 10:28:58 -0300

    creacion de las clases GestorEmpleados y Impuestos

---
+++ LAB-2/Empleados/src/GestorEmpleados.java
@@ -0,0 +1,26 @@
+import java.util.ArrayList;
+
+public class GestorEmpleados {
+    ArrayList<Empleado> empleados;
+
+    public GestorEmpleados(){
+        empleados = new ArrayList<Empleado>();
+    }
+
+    public void addEmpleado(Empleado empleado) {
+        // metodo para agregar un empleado al array.
+        empleados.add(empleado);
+    }
+    public void removeEmpleado(int id) {    // Metodo que elimina un empleado del array, usa un for loop para encontrar
+                                            // la ID a remover, y se rompe para salir del loop.
+        for ( Empleado EMPLEADO : empleados) {
+            // variable de tipo Empleado recorre el array de tipo Empleado
+            if (EMPLEADO.getId() == id) {
+                empleados.remove(EMPLEADO);
+                break;
+            }
+        }
+    }
+    public void editEmpleado(String nombre, )
+
+}
---
+++ LAB-2/Empleados/src/Impuesto.java
@@ -0,0 +1,3 @@
+public interface Impuesto  {
+    public double calcularImpuesto();
+}
