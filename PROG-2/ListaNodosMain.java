d6af613b3ff2f100bd48f7ab845b85485d02a67b
louisrubin <miguel10.rbn@gmail.com>
2023-10-17 21:58:07 -0300

    case 5: Buscar Valor
    funcion que devuelve si un valor existe en la lista

--- PROG-2/ListaNodosMain.java
+++ PROG-2/ListaNodosMain.java
@@ -5,10 +5,14 @@ public class ListaNodosMain {
         Scanner sc = new Scanner(System.in);
         Lista lista = new Lista();
 
+        lista.agregarFinal(3);  // para ir probando
+        lista.agregarFinal(4);
+        lista.agregarFinal(5);
+
         while (true) {
             System.out.println("-------------------------------");
             System.out.print(" 1- Agregar al Inicio\n 2- Imprimir Lista\n 3- Agregar al Final\n" +
-                    " 4- Ultimo Valor\n 9- SALIR\n --> ");
+                    " 4- Ultimo Valor\n 5- Existe Valor\n 9- SALIR\n --> ");
             int opc = sc.nextInt();
             sc.nextLine();
             System.out.println("-------------------------------");
@@ -17,8 +21,8 @@ public class ListaNodosMain {
                 case 1:
                     System.out.print("Nuevo Valor: ");
                     int valor = sc.nextInt();
-                    lista.agregarInicio(valor);
                     sc.nextLine();
+                    lista.agregarInicio(valor);
                     break;
 
                 case 2:
@@ -28,13 +32,20 @@ public class ListaNodosMain {
                 case 3:
                     System.out.print("Nuevo Valor: ");
                     valor = sc.nextInt();
-                    lista.agregarFinal(valor);
                     sc.nextLine();
+                    lista.agregarFinal(valor);
                     break;
 
                 case 4:
                     if (lista.ultNodo == null) System.out.println("\t\tLista vacia");   // si no hay datos agregados
-                    else System.out.println(lista.ultNodo.dato);
+                    else System.out.println("Ult.: "+ lista.ultNodo.dato);
+                    break;
+
+                case 5:
+                    System.out.print("Valor a buscar: ");
+                    valor = sc.nextInt();
+                    sc.nextLine();
+                    lista.existeValor(valor);
                     break;
 
                 case 9:
@@ -48,7 +59,7 @@ public class ListaNodosMain {
 }
 
 class Lista {
-    Nodo primerNodo, ultNodo; //nuevoNodo;
+    Nodo primerNodo, ultNodo;
 
     public Lista(){
         // ..
@@ -72,7 +83,7 @@ class Lista {
             return;
         }
 
-        Nodo actual = primerNodo;
+        Nodo actual = primerNodo;   // puntero
         Nodo nuevoNodo = new Nodo(valor);
 
         while (true){   // saldra del while en el IF
@@ -81,9 +92,28 @@ class Lista {
                 ultNodo = nuevoNodo;
                 return;
             }
-            actual = actual.sig;
+            actual = actual.sig;        // avanza una posicion
+        }
+    }
+
+    public boolean existeValor(int valorParam){
+        if (primerNodo == null){
+            System.out.println("\t\tLista vacia");
+            return false;
         }
 
+        Nodo actual = primerNodo;   // puntero
+
+        while (actual != ultNodo.sig){   // saldra del while en el IF
+            if (actual.dato == valorParam) {
+                System.out.println("Si existe.");
+                return true;
+            }
+            actual = actual.sig;        // avanza una posicion
+
+        }
+        System.out.println("No existe.");
+        return false;
     }
 
     public void imprimirLista(){
@@ -92,13 +122,12 @@ class Lista {
             return;
         }
 
-        Nodo actual = primerNodo;
+        Nodo actual = primerNodo;   // puntero
         while (actual.sig != null){
             System.out.print(actual.dato + ", ");
             actual = actual.sig;
-
         }
-        System.out.println(actual.dato);
+        System.out.println(actual.dato);    // imprime el ultimo valor
     }
 
 }
