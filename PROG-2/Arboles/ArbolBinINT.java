c3afe39b990b3d9eeba8277fc756d43def61fe46
Luis Rubin <72027738+louisrubin@users.noreply.github.com>
2023-11-02 10:43:55 -0300

    imprimir arbol (pre, in, post -> orden)

--- PROG-2/Arboles/ArbolBinINT.java
+++ PROG-2/Arboles/ArbolBinINT.java
@@ -1,11 +1,12 @@
 package Arboles;
 
-public class ArbolBinINT {     //
+public class ArbolBinINT {
     NodoBinInt raiz;
 
     public ArbolBinINT(){
         raiz = null;
     }
+
     public ArbolBinINT(NodoBinInt raiz){
         // instancia el arbol con una raiz especifica
         this.raiz = raiz;
@@ -14,24 +15,69 @@ public class ArbolBinINT {     //
     public static void main(String[] args) {
         ArbolBinINT arbolBin = new ArbolBinINT();
 
+        // carga de valores
+        arbolBin.agregar(4);
+        arbolBin.agregar(2);
+        arbolBin.agregar(3);
+        arbolBin.agregar(1);
+        arbolBin.agregar(5);
+        arbolBin.agregar(6);
 
+        arbolBin.imprPostOrd(arbolBin.raiz);
     }
 
-    public NodoBinInt agregarRecursiv(NodoBinInt nodoParm, int valor){
+    public void agregar(int valor){
+        this.raiz = agregarRecursiv(this.raiz, valor);  // la raiz pq tiene que comparar con ese
+    }
+
+    private NodoBinInt agregarRecursiv(NodoBinInt nodoVerif, int valor){
         // Si el nodo es una hoja, entonces lo crear y retorna
-        if (nodoParm == null){
-            nodoParm = new NodoBinInt(valor);
-            return nodoParm;
+        if (nodoVerif == null){
+            nodoVerif = new NodoBinInt(valor);
+            return nodoVerif;
         }
 
         // En caso contrario, recorrer el arbol de forma recursiva
-        if (nodoParm.data <= 0 ) {
-            nodoParm.nodoIzq = agregarRecursiv(nodoParm.nodoIzq, valor);
+        if (valor < nodoVerif.data ) {
+            nodoVerif.nodoIzq = agregarRecursiv(nodoVerif.nodoIzq, valor);    // recursivo
         }
 
-        else if (nodoParm.data > 0) {
-            nodoParm.nodoDer = agregarRecursiv(nodoParm.nodoDer, valor);
+        else if (valor >= nodoVerif.data) {
+            nodoVerif.nodoDer = agregarRecursiv(nodoVerif.nodoDer, valor);    // recursivo
         }
-        return nodoParm;
+        return nodoVerif;
+    }
+
+    public void imprPreOrd(NodoBinInt nodo){  // pre-orden
+        /*if (this.raiz == null) {
+            System.out.printf("Vacío.");
+            return;
+        }*/
+        System.out.print(nodo.data + ", ");
+        if (nodo.nodoIzq != null){
+            imprPreOrd(nodo.nodoIzq);
+        }
+        if (nodo.nodoDer != null){
+            imprPreOrd(nodo.nodoDer);
+        }
+    }
+
+    public void imprInOrd(NodoBinInt nodo){    // in-orden
+        if (nodo.nodoIzq != null){
+            imprInOrd(nodo.nodoIzq);
+        }
+        System.out.print(nodo.data + ", ");
+        if (nodo.nodoDer != null){
+            imprInOrd(nodo.nodoDer);
+        }
+    }
+    public void imprPostOrd(NodoBinInt nodo){   // post-orden
+        if (nodo.nodoIzq != null){
+            imprPostOrd(nodo.nodoIzq);
+        }
+        if (nodo.nodoDer != null){
+            imprPostOrd(nodo.nodoDer);
+        }
+        System.out.print(nodo.data + ", ");
     }
 }
